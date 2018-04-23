2c43aad47e163e6f54e04cf90ef03c46949bd68b
Nick Liu <deltik@gmx.com>
2018-04-23 07:33:26 -0500

    VisualDC: Initial support for named EnclosureRacks
    - NEW: (WIP) EnclosureRack name written on top of its box

--- src/components/lib/VisualDC/EnclosureRack.vue
+++ src/components/lib/VisualDC/EnclosureRack.vue
@@ -1,21 +1,35 @@
 <template>
   <Box :options="meshOptions" :position="position" :key="id">
     <Material
-      diffuse="#78909C"
+      v-model="material"
       :specular="[0.1, 0.1, 0.1]"
       emissive="#000000"
-    />
+    >
+      <Texture v-model="texture" :key="id" />
+    </Material>
   </Box>
 </template>
 
 <script>
-  let { Vector3, Vector4 } = require('vue-babylonjs/classes');
+  let { Vector3, Vector4, DynamicTexture } = require('vue-babylonjs/classes');
+
+  // Begin: Vue-BabylonJS quirks
+  import Texture from 'vue-babylonjs/lib/texture'
+  // Texture needs to be a DynamicTexture
+  Texture.methods.create = function() {
+    let texture = this.value || new DynamicTexture(this.name, 512, this.$scene);
+    this.$replace(texture);
+  }
+  // End: Vue-BabylonJS quirks
 
   export default {
     name: "EnclosureRack",
     data() {
       return {
-        scale: 0.8
+        scale: 0.8,
+        scene: null,
+        material: null,
+        texture: null
       }
     },
     props: {
@@ -44,6 +58,9 @@
       id() {
         return this.data['id']
       },
+      name() {
+        return this.data['name']
+      },
       height() {
         return this.data['height']
       },
@@ -55,6 +72,16 @@
       },
       position() {
         return new Vector3(-(this.x + 0.5), this.relativeHeight / 2, this.y + 0.5)
+      }
+    },
+    watch: {
+      texture() {
+        let highlight = false
+        let noHighlightColor = "#78909C";
+        let yesHighlightColor = "#8ba3af";
+        let highlightColor = highlight ? yesHighlightColor : noHighlightColor;
+        this.texture.drawText(this.name, null, null, "160px 'Roboto Condensed'", "white", highlightColor)
+        this.texture.wAng = Math.PI / 180 * 90
       }
     }
   }
